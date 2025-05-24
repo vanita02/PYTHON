b387a10c7282e37fde1ff28ceaa0e7298b6039cf
Vanita <140881033+vanita02@users.noreply.github.com>
2025-05-24 16:50:23 +0530

    Add files via upload

---
+++ LAB/Python Programming - Lab - 11/Calculater.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print('addition : ',a+b)
+
+def sub(a,b):
+    print('substaction : ',a-b)
+
+def mul(a,b):
+    print('multiplication : ',a*b)
+
+def div(a,b):
+    print('division : ',a/b)
